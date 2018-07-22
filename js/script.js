3220f0483af84721a658c2aa6b083274f6daacdf
Shakeel <shakeel.ahmed@b60apps.co.uk>
2018-07-22 10:45:28 +0100

    Calculator
    V1

---
+++ js/script.js
@@ -0,0 +1,116 @@
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+}
+
+function inputDecimal(dot) {
+	if (calculator.waitingForSecondOperand === true) return;
+
+  // If the `displayValue` does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand)  {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+  if (firstOperand == null) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const currentValue = firstOperand || 0;
+    const result = performCalculation[operator](currentValue, inputValue);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  if(nextOperator === '√'){
+    calculator.displayValue = String(Math.sqrt(inputValue));
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '/': (a, b) => a / b,
+  '*': (a, b) => a * b,
+  '%': (a, b) => a % b,
+  'ⁿ': (a, b) => Math.pow(a, b),
+  '=': (a, b) => b
+};
+
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
+
+function updateDisplay() {
+  const display = document.querySelector('.display');
+  display.value = calculator.displayValue;
+}
+
+function updateOperator(){
+  const display = document.querySelector('.display-operator');
+  display.value = calculator.operator;
+}
+
+updateDisplay();
+updateOperator();
+
+const keys = document.querySelector('.num-pads');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+		updateDisplay();
+    updateOperator();
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+		updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('clear')) {
+    resetCalculator();
+		updateDisplay();
+    updateOperator();
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+  updateOperator();
+});
